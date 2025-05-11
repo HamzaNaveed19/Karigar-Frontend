2686a35ae1335a8b6ec91eb03f4686ea0ddc3c54
HamzaNaveed19 <hamzanaveed981@gmail.com>
2025-05-11 23:08:52 +0500

    added OTP

--- src/Pages/ProviderSetup.jsx
+++ src/Pages/ProviderSetup.jsx
@@ -795,12 +795,6 @@ const ProviderSetup = () => {
               <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
                 <dl className="sm:divide-y sm:divide-gray-200">
                   <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                    <dt className="text-sm font-medium text-gray-500">Full name</dt>
-                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                      {userId || "User"}
-                    </dd>
-                  </div>
-                  <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                     <dt className="text-sm font-medium text-gray-500">Profession</dt>
                     <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                       {formData.profession || "Not specified"}
